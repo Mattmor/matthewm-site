5991d5275d4d1c70dfd2e8d69a45c0d53524d3e9
Matt <dozy-morgan@hotmail.co.uk>
2015-12-13 18:50:17 +0000

    Changed color of blog href in footer.

--- resources/views/blog/footer/_footer.blade.php
+++ resources/views/blog/footer/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-6 col-sm-6 col-sm-offset-3 col-md-offset-3 blog-footer">
             <a href="/" class="blog-footer-text">HOME</a>
-            <a href="/blog" class="blog-footer-text-active">BLOG</a>
+            <a href="/blog" class="blog-footer-text">BLOG</a>
             <a href="/tools" class="blog-footer-text">WEB TOOLS</a>
             <a href="/offline" class="blog-footer-text">OFFLINE PROJECTS</a>
             <a href="/links" class="blog-footer-text">LINKS</a>
