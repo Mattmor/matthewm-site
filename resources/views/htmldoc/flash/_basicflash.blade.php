1ae12e4a570dd6a7040557c19641644345a6c646
mattmor <dozy-morgan@hotmail.co.uk>
2016-01-21 21:07:36 +0000

    Hotfixed the flash message. It displays on screen now (badly)

--- resources/views/htmldoc/flash/_basicflash.blade.php
+++ resources/views/htmldoc/flash/_basicflash.blade.php
@@ -1,7 +1,7 @@
 @if (Session::has('flash_message'))
     <div class="container">
         <div class="row">
-            <div class="alert alert-info alert-dismissible flash-bottom" role="alert">
+            <div class="col-md-8 alert alert-info alert-dismissible flash-bottom" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 {{ Session::get('flash_message') }}
             </div>
@@ -10,7 +10,7 @@
 @elseif(Session::has('flash_message_danger'))
     <div class="container">
         <div class="row">
-            <div class="alert alert-danger alert-dismissible flash-bottom" role="alert">
+            <div class="col-md-8 alert alert-danger alert-dismissible flash-bottom" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 {{ Session::get('flash_message_danger') }}
             </div>
